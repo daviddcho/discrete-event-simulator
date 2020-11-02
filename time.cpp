37ed756f9aed60634c7ee02177c962226b60029d
daviddcho <amazingdcho@gmail.com>
2020-11-01 19:39:15 -0500

    Finishing touches with comments

--- time.cpp
+++ time.cpp
@@ -1,13 +1,17 @@
 #include "simulation.h"
 
+
+// Global current time
 int current_time = getINIT_TIME();
 
 
+// Returns the current time
 int getCurrentTime() {
   return current_time;
 }
 
 
+// Sets current time to given time and returns current time
 int setCurrentTime(int time) {
   current_time = time;  
   return current_time;
